29f5bde951beb4c1215897e1e8a6479c9c5fa079
Alexey Sachko <alexey.sachko@gmail.com>
2020-02-01 16:58:34 +0500

    Added error handling

--- src/middleware/errorHandlers.ts
+++ src/middleware/errorHandlers.ts
@@ -1,12 +1,8 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { HTTPClientError, HTTP404Error } from "../utils/httpErrors";
+import * as ErrorHandler from "../utils/ErrorHandler";
 
-type ExtendedError = {
-  status: number;
-} & Error;
-
-type ErrorHandler = (
-  err: ExtendedError,
+type FullHanler = (
+  err: ErrorHandler.ExtendedError,
   req: Request,
   res: Response,
   next: NextFunction
@@ -14,30 +10,20 @@ type ErrorHandler = (
 
 const handle404Error = (router: Router) => {
   router.use((req, res) => {
-    throw new HTTP404Error("Method not found.");
+    ErrorHandler.notFoundError();
   });
 };
 
 const handleClientErrors = (router: Router) => {
   router.use(((err, req, res, next) => {
-    if (err instanceof HTTPClientError) {
-      console.error(err);
-      res.status(err.status).send(err.message);
-    } else {
-      next(err);
-    }
-  }) as ErrorHandler);
+    ErrorHandler.clientError(err, res, next);
+  }) as FullHanler);
 };
 
 const handleServerErrors = (router: Router) => {
   router.use(((err, req, res, next) => {
-    console.error(err);
-    if (process.env.NODE_ENV === "production") {
-      res.status(500).send("Internal Server Error");
-    } else {
-      res.status(500).send(err.stack);
-    }
-  }) as ErrorHandler);
+    ErrorHandler.serverError(err, res, next);
+  }) as FullHanler);
 };
 
 export default [handle404Error, handleClientErrors, handleServerErrors];
--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,7 @@ import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import routes from "./services";
-
-const router = express();
-applyMiddleware(middleware, router);
-applyRoutes(routes, router);
-
-const { PORT = 3000 } = process.env;
-const server = http.createServer(router);
-
-server.listen(PORT, () =>
-  console.log(`Server is running http://localhost:${PORT}...`)
-);
+import errorHandlers from "./middleware/errorHandlers";
 
 process.on("uncaughtException", e => {
   console.log(e);
@@ -23,3 +13,15 @@ process.on("unhandledRejection", e => {
   console.log(e);
   process.exit(1);
 });
+
+const router = express();
+applyMiddleware(middleware, router);
+applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
+
+const { PORT = 3000 } = process.env;
+const server = http.createServer(router);
+
+server.listen(PORT, () =>
+  console.log(`Server is running http://localhost:${PORT}...`)
+);
