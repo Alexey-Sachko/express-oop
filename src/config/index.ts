83035398a8a8aac3f06d2f9d0675a934b1733c3f
Alexey Sachko <alexey.sachko@gmail.com>
2020-02-09 21:42:16 +0500

    Removed constants from config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const { env } = process;
 
 const config = {
   email: {
-    address: "resnicyekb@yandex.ru",
-    password: "ksushaSokol1998"
+    address: env.OWN_EMAIL || "",
+    password: env.OWN_EMAIL_PASSWORD || ""
   },
   token: {
     secret: env.JWT_SECRET || "exampleSecret"
